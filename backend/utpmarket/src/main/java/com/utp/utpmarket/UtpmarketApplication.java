5b723a1ec6f9878b9811cbb017214c956964c1ae
IJosueeh01 <royer.tanta27@gmail.com>
2025-08-15 12:53:22 -0500

    Init: Inicializando proyectos

---
+++ backend/utpmarket/src/main/java/com/utp/utpmarket/UtpmarketApplication.java
@@ -0,0 +1,13 @@
+package com.utp.utpmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtpmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtpmarketApplication.class, args);
+	}
+
+}
---
+++ backend/utpmarket/src/test/java/com/utp/utpmarket/UtpmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.utpmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtpmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
