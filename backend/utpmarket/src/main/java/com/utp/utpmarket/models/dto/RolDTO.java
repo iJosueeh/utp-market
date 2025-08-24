c0b9d340e75cc81b228ccedeed178055920bd218
IJosueeh01 <royer.tanta27@gmail.com>
2025-08-24 16:01:01 -0500

    feat: Entidades Categoria, EstadoPedido, Producto, Rol y DTOs añadidas.

---
+++ backend/utpmarket/src/main/java/com/utp/utpmarket/models/dto/RolDTO.java
@@ -0,0 +1,15 @@
+package com.utp.utpmarket.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RolDTO {
+    private Long id;
+    private String nombre;
+}
