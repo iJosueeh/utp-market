e15b891812a226ff61c990efb911a02c2403133b
IJosueeh01 <royer.tanta27@gmail.com>
2025-08-23 23:56:25 -0500

    feat: agregar logica de negocio de login y registre base

---
+++ backend/utpmarket/src/main/java/com/utp/utpmarket/models/dto/usuario/UsuarioResponseDTO.java
@@ -0,0 +1,8 @@
+package com.utp.utpmarket.models.dto.usuario;
+
+public class UsuarioResponseDTO {
+    private Long id;
+    private String nombre;
+    private String email;
+    private boolean estado;
+}
