79057413a1892f1bdc5835d9c98efd3e9a2d94e6
KennySth <kenny_sthiven@outlook.com>
2025-08-28 21:32:13 -0500

    feat: Componente footer creado.

--- frontend/UTP-Market/src/pages/MainPage.jsx
+++ frontend/UTP-Market/src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/common/Navbar";
 import FAQList from "../components/a/FAQList";
-
+import Footer from "../components/common/Footer";
 
 
 export default function MainPage() {
@@ -19,9 +19,9 @@ export default function MainPage() {
              <section className="bg-light py-5"> <FAQList /></section>  
             </main>
 
-            <footer className="main-footer">
-                
-            </footer>
+            
+            <Footer/>
+            
         </div>
         </>
     );
