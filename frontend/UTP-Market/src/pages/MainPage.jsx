f23a843305bc73d28e43619cac4a27b93e23c84f
ian101710 <ianengel72@gmail.com>
2025-08-30 00:16:54 -0500

    feat: hero creado

--- frontend/UTP-Market/src/pages/MainPage.jsx
+++ frontend/UTP-Market/src/pages/MainPage.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Navbar from "../components/common/Navbar";
 import FAQList from "../pages/features/faq/FAQList";
 import CardProfileTeam from "../pages/features/CardProfileTeam";
+import Hero from "../pages/features/hero/Hero"
 export default function MainPage() {
     return (
         <>
             <div className="main-page container-fluid px-0">
-                <div className="px-3">
+                <div>
                     <Navbar />
                     <header className="main-header">
                     </header>
+                       <Hero />
+
 
                     <main className="content">
                         <section className="bg-light py-5 mt-5">
---
+++ frontend/UTP-Market/src/pages/features/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <h1>Bienvenido a UTP Market</h1>
+            <p>Encuentra los mejores productos y servicios aquí.</p>
+        </section>
+    );
+};
+
+export default Hero;
