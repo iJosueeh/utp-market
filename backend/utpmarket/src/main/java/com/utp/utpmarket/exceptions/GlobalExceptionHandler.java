44ed02639bedcd2084b18665765513a31b59ead4
IJosueeh01 <royer.tanta27@gmail.com>
2025-09-25 22:13:50 -0500

    feat: Mejoramiento de codigo limpio e escalable

---
+++ backend/utpmarket/src/main/java/com/utp/utpmarket/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+
+package com.utp.utpmarket.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<String> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<String> handleInvalidCredentialsException(InvalidCredentialsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
