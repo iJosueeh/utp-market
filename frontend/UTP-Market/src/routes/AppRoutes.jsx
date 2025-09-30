1cf14dcf2e94121a155df90306ea66a402108e43
AlexanderSc21 <alexandersinte21@gmail.com>
2025-09-29 23:00:16 -0500

    feat/error resuelto

--- frontend/UTP-Market/src/routes/AppRoutes.jsx
+++ frontend/UTP-Market/src/routes/AppRoutes.jsx
@@ -1,29 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
 import AUPage from '../pages/AboutUs/AUPage';
 import HelpPage from '../pages/Help/HelpPage';
 import MainPage from '../pages/MainPage';
-import ProductPage from '../pages/ProductPage'
-import ProductDetail from '../pages/ProductDetail';
+import ProductPage from '../pages/ProductPage';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Página por defecto */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
-        {/* Rutas públicas */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Navigate to="/main" replace />} />
 
         {/* Ruta para la página "Acerca de nosotros" */}    
-      <Route path="/about-us" element={<AUPage />} />
+        <Route path="/about-us" element={<AUPage />} />
         {/* Ruta para la página "Centro de ayuda" */}
-      <Route path="/help" element={<HelpPage />} />
-      <Route path="/main" element={<MainPage />} />
-      <Route path="/products" element={<ProductPage />} />
+        <Route path="/help" element={<HelpPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/products" element={<ProductPage />} />
         {/* Puedes agregar rutas protegidas aquí más adelante */}
         {/* <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} /> */}
 
